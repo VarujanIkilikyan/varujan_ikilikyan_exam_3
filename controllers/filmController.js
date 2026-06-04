fe41823707768d524da8b74b3745bc875181ae76
Varujan Ikilikyan <varujanna@gmail.com>
2026-06-04 04:55:24 +0400

    add serch

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -1,2 +1,43 @@
 import HttpErrors from 'http-errors';
 import {FilmsModel} from '../models/Index.model.js';
+import {Op}  from 'sequelize'
+
+export default {
+    async search (req, res, next) {
+        try {
+            const {search,page} = req.query;
+
+            const limitNum = 10;
+            const pageNum = Math.max(1, parseInt(page) || 1);
+            const offset = Math.ceil((pageNum - 1) * limitNum);
+
+            const whereCondition = {};
+
+            if (search && search.trim() !== '') {
+                whereCondition.title = {
+                    [Op.like]: `%${search}%`
+                };
+            }
+
+            const {count,rows} = await FilmsModel.findAndCountAll({
+                where: whereCondition,
+                limit: limitNum,
+                offset:offset,
+                order: [['title', 'ASC']]
+            });
+
+            res.json({
+                message: 'get all users',
+                FilmList:rows,
+                pagination: {
+                    "currentPage": pageNum,
+                    "totalPages": Math.ceil(count / limitNum),
+                    "totalUsers": count,
+                    "UsersPerPage": limitNum,
+                }
+            })
+        }catch (e){
+            next(e);
+        }
+}
+}
--- routes/films.js
+++ routes/films.js
@@ -1,5 +1,14 @@
 import {Router} from "express";
 
+import validate from "../middlewares/validation.js";
+import schemas from "../middlewares/schemas/auth.schema.js";
+import authorization from "../middlewares/authorization.js";
+
+import controller from "../controllers/filmController.js";
+
+
 const filmsRoutes = Router();
+
+filmsRoutes.get('/',authorization,controller.search);
 
 export default filmsRoutes;
